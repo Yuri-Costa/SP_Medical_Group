496765e350ae6400ee9a480618e2f96a2cba8b23
yuri costa <yuri342costa@gmail.com>
2021-06-30 21:42:52 -0300

    finish

--- API/Sp_Medical_Group/Sp_Medical_Group/Contexts/MedicalGroupContext.cs
+++ API/Sp_Medical_Group/Sp_Medical_Group/Contexts/MedicalGroupContext.cs
@@ -32,7 +32,7 @@ namespace Sp_Medical_Group.Contexts
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=LAB08DESK1501; initial catalog=SP_MEDICAL_GROUP; user Id=sa; pwd=buck0315;");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-5GTFAN8; initial catalog=SP_MEDICAL_GROUP; user Id=yuri; pwd=buck0315;");
             }
         }
 
